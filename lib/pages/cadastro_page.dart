3046b58085739e9d2edd2ea1528f0992ab8a6558
DAVI GUEDES VENANCIO DA SILV <25163231@edu119.local>
2026-08-17 09:40:17 -0300

    finalizando projeto da tela de login em flutter

---
+++ lib/pages/cadastro_page.dart
@@ -0,0 +1,210 @@
+import '../dados_mock.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class CadastroPage extends StatefulWidget {
+  const CadastroPage({super.key});
+
+  @override
+  State<CadastroPage> createState() => _CadastroPageState();
+}
+
+class _CadastroPageState extends State<CadastroPage> {
+  final TextEditingController nomeController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController senhaController = TextEditingController();
+  final TextEditingController confirmarSenhaController =
+      TextEditingController();
+  bool esconderSenha = false;
+  bool esconderConfirmacao = false;
+
+  void cadastrar() {
+    String nome = nomeController.text.trim();
+    String email = emailController.text.trim();
+    String senha = senhaController.text;
+    String confirmarSenha = confirmarSenhaController.text;
+
+    if (nome.isEmpty ||
+        email.isEmpty ||
+        senha.isEmpty ||
+        confirmarSenha.isEmpty) {
+      mostrarMensagem("Preencha todos os campos");
+      return;
+    }
+    if (!email.contains("@")) {
+      mostrarMensagem("Digite um e-mail valido");
+      return;
+    }
+
+    if (senha.length < 4) {
+      mostrarMensagem(
+        "A senha é muito curta, digite uma senha com mais de 4 caracteres",
+      );
+      return;
+    }
+
+    if (senha != confirmarSenha) {
+      mostrarMensagem("As senhas não são iguais");
+      return;
+    }
+
+    bool emailExiste = false;
+
+    for (var usuario in usuarios) {
+      if (usuario['email'] == email) {
+        emailExiste = true;
+        return;
+      }
+    }
+
+    if (emailExiste) {
+      mostrarMensagem("Já existe um usuário com esse e-mail");
+      return;
+    }
+
+    usuarios.add({"nome": nome, "email": email, "senha": senha});
+    mostrarMensagem("Usuário Cadastrado com sucesso");
+  }
+
+  void mostrarMensagem(String mensagem) {
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar((SnackBar(content: Text(mensagem))));
+  }
+
+  @override
+  void dispose() {
+    nomeController.dispose();
+    emailController.dispose();
+    senhaController.dispose();
+    confirmarSenhaController.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Criar usuário'), centerTitle: true),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(height: 15),
+
+            const Icon(Icons.person_add, size: 90),
+
+            const SizedBox(height: 15),
+
+            const Text(
+              "Criar Conta",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+            ),
+
+            const SizedBox(height: 15),
+
+            TextField(
+              controller: nomeController,
+              decoration: const InputDecoration(
+                labelText: "Nome",
+                hintText: "Digite seu nome",
+                prefixIcon: Icon(Icons.person),
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            const SizedBox(height: 15),
+
+            TextField(
+              controller: emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                labelText: "E-mail",
+                hintText: "Digite seu e-mail",
+                prefixIcon: Icon(Icons.email),
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            const SizedBox(height: 15),
+
+            TextField(
+              controller: senhaController,
+              obscureText: esconderSenha,
+              decoration: InputDecoration(
+                labelText: "Senha",
+                hintText: "Digite sua senha",
+                prefixIcon: Icon(Icons.block),
+                border: OutlineInputBorder(),
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      esconderSenha = !esconderSenha;
+                    });
+                  },
+                  icon: Icon(
+                    esconderSenha ? Icons.visibility : Icons.visibility_off,
+                  ),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 15),
+
+            TextField(
+              controller: confirmarSenhaController,
+              obscureText: esconderConfirmacao,
+              decoration: InputDecoration(
+                labelText: "Confirmar senha",
+                hintText: "Digite sua senha",
+                prefixIcon: const Icon(Icons.lock_outline),
+                border: OutlineInputBorder(),
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      esconderConfirmacao = !esconderConfirmacao;
+                    });
+                  },
+                  icon: Icon(
+                    esconderConfirmacao
+                        ? Icons.visibility
+                        : Icons.visibility_off,
+                  ),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            ElevatedButton.icon(
+              onPressed: cadastrar,
+              icon: const Icon(Icons.person_add, size: 18),
+              label: const Text("Cadastrar", style: TextStyle(fontSize: 14)),
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ),
+                minimumSize: Size.zero,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 10),
+            OutlinedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("voltar para o Login"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
